44ca1dfea5a6d3a116bacc35e6c234aaa2d842e3
xanderyeng <ckalex54@gmail.com>
2026-02-22 21:29:48 +0300

    Fix: Updated the correct import

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { About } from './components/About';
 import { Portfolio } from './components/Portfolio';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
